c53a81cb9eb8aa57bb79ea0578f16ec66e31fb69
itssayushhere <yayushman088@gmail.com>
2024-06-25 07:10:21 +0530

    Vercel routes uploaded

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -13,44 +13,49 @@ import Medicine from "../pages/Medicine";
 import Checkup from "../pages/medicalserviecs/Checkup";
 import MyAccount from "../Dashboard/user_account/MyAccount";
 import Dashboard from "../Dashboard/doctor-account/Dashboard";
-import ProtectedRoute from "../components/ProtectedRoute";
+import ProtectedRoute from "../routes/Protectedroute.jsx";
 import MentalHealth from "../pages/MentalHealth";
 import Success from "../pages/Success";
-// import { AuthContextProvider } from "../context/AuthContext";
+import { AuthContextProvider } from "../context/AuthContext";
 
 const Routers = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/register" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/doctors" element={<Doctors />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/doctors/:id" element={<DoctorsDetails />} />
-      <Route path="/community" element={<Community />} />
-      <Route path="/medicine" element={<Medicine />} />
-      <Route path="/checkup" element={<Checkup />} />
-      <Route path="/mentalhealth" element={<MentalHealth />} />
-      <Route
-        path="/users/profile/me"
-        element={
-          <ProtectedRoute allowedRoles={["patient"]}>
-            <MyAccount />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/doctors/profile/me"
-        element={
-          <ProtectedRoute allowedRoles={["doctor"]}>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/users/profile/me/checkout_success" element={<Success />} />
-    </Routes>
+    <AuthContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/doctors/:id" element={<DoctorsDetails />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/medicine" element={<Medicine />} />
+        <Route path="/checkup" element={<Checkup />} />
+        <Route path="/mentalhealth" element={<MentalHealth />} />
+        <Route
+          path="/users/profile/me"
+          element={
+            <ProtectedRoute allowedRoles={["patient"]}>
+              <MyAccount />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/doctors/profile/me"
+          element={
+            <ProtectedRoute allowedRoles={["doctor"]}>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/users/profile/me/checkout_success"
+          element={<Success />}
+        />
+      </Routes>
+    </AuthContextProvider>
   );
 };
 
