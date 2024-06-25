4b11bb60951e24050f91ec4af714d22beed1afd2
itssayushhere <yayushman088@gmail.com>
2024-06-25 05:38:55 +0530

    Commit

--- frontend/src/routes/Protectedroute.jsx
+++ frontend/src/routes/Protectedroute.jsx
@@ -1,18 +1,14 @@
+// components/ProtectedRoute.js
 import { useContext } from "react";
 import { authContext } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
   const { token, role } = useContext(authContext);
-  
-  // Check if the user's role is included in the allowedRoles array
+
   const isAllowed = allowedRoles.includes(role);
 
-  // If user has token and isAllowed, render the protected route
-  // Otherwise, redirect to login page
-  const accessibleRoute = token && isAllowed ? children : <Navigate to="/login" replace={true} />;
-  
-  return accessibleRoute;
+  return token && isAllowed ? children : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,4 +1,6 @@
+// routes/Routers.js
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Contact from "../pages/Contact";
 import Login from "../pages/Login";
@@ -7,55 +9,53 @@ import Doctors from "../pages/Doctors/Doctors";
 import DoctorsDetails from "../pages/Doctors/DoctorsDetails";
 import Signup from "../pages/Signup";
 import Community from "../pages/Community";
-import { Routes, Route } from "react-router-dom";
 import Medicine from "../pages/Medicine";
 import Checkup from "../pages/medicalserviecs/Checkup";
 import MyAccount from "../Dashboard/user_account/MyAccount";
 import Dashboard from "../Dashboard/doctor-account/Dashboard";
-import ProtectedRoute from "./Protectedroute";
+import ProtectedRoute from "../components/ProtectedRoute";
 import MentalHealth from "../pages/MentalHealth";
 import Success from "../pages/Success";
 import { AuthContextProvider } from "../context/AuthContext";
+
 const Routers = () => {
   return (
-    <>
-      <AuthContextProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/register" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/doctors" element={<Doctors />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/doctors/:id" element={<DoctorsDetails />} />
-          <Route path="/community" element={<Community />} />
-          <Route path="/medicine" element={<Medicine />} />
-          <Route path="/checkup" element={<Checkup />} />
-          <Route path="/mentalhealth" element={<MentalHealth />} />
-          <Route
-            path="/users/profile/me"
-            element={
-              <ProtectedRoute allowedRoles={["patient"]}>
-                <MyAccount />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/doctors/profile/me"
-            element={
-              <ProtectedRoute allowedRoles={["doctor"]}>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/users/profile/me/checkout_successs"
-            element={<Success />}
-          />
-        </Routes>
-      </AuthContextProvider>
-    </>
+    <AuthContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/doctors/:id" element={<DoctorsDetails />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/medicine" element={<Medicine />} />
+        <Route path="/checkup" element={<Checkup />} />
+        <Route path="/mentalhealth" element={<MentalHealth />} />
+        <Route
+          path="/users/profile/me"
+          element={
+            <ProtectedRoute allowedRoles={["patient"]}>
+              <MyAccount />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/doctors/profile/me"
+          element={
+            <ProtectedRoute allowedRoles={["doctor"]}>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/users/profile/me/checkout_success"
+          element={<Success />}
+        />
+      </Routes>
+    </AuthContextProvider>
   );
 };
 
