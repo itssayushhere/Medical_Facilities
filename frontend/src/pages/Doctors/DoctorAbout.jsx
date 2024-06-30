66d92b6fa7dc2541491ce909b5b3990e621d7897
itssayushhere <yayushman088@gmail.com>
2024-06-30 15:16:39 +0530

    updated

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -15,7 +15,7 @@ const DoctorAbout = ({ name, about, qualifications, experiences }) => {
       </div>
 
       <div className="mt-12">
-        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-bold">
           Education
         </h3>
         <ul className="pt-4 md:p-5">
@@ -40,7 +40,7 @@ const DoctorAbout = ({ name, about, qualifications, experiences }) => {
       </div>
 
       <div className="mt-12">
-        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-bold">
           Experience
         </h3>
         <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
