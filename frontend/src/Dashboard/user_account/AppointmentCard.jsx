2a10fb3d32f9e171b3ed059d6558e49613ed795f
itssayushhere <yayushman088@gmail.com>
2024-07-07 21:57:59 +0530

    updated

--- frontend/src/Dashboard/user_account/AppointmentCard.jsx
+++ frontend/src/Dashboard/user_account/AppointmentCard.jsx
@@ -1,32 +1,43 @@
-import React from "react";
+/* eslint-disable react/prop-types */
 import defaultphoto from "../../assets/images/user.webp";
 import videoIcon from "../../assets/images/video-icon.webp";
+import { formatDate } from "../../utils/formatDate";
 
-const AppointmentCard = () => {
+const AppointmentCard = ({data}) => {
   return (
     <div className=" border    rounded-2xl bg-white  shadow-xl ">
       <div className="">
         <div className="flex justify-between bg-sky-200 items-center gap-2 m-3 p-2 border-b-2 border-gray-600 rounded-xl ">
           <div className="flex items-center justify-start gap-1 ">
-            <img
-              src={defaultphoto}
-              alt="default"
-              className="booking_img rounded-full"
-              width={40}
-              height={20}
-            />
+            {data.doctor.photo ? (
+              <img
+                src={data.doctor.photo}
+                alt="Doctor photo"
+                className="booking_img rounded-full"
+                width={40}
+                height={20}
+              />
+            ) : (
+              <img
+                src={defaultphoto}
+                alt="Doctor photo"
+                className="booking_img rounded-full"
+                width={40}
+                height={20}
+              />
+            )}
             <div>
-              <p>Ayushman Yadav</p>
+              <p>{data.doctor.name}</p>
             </div>
           </div>
           <div className="bg-[#CCF0F3] text-irisBlueColor py-1 px-2 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded mr-2">
-            <p>specialization</p>
+            <p>{data.doctor.specialization}</p>
           </div>
         </div>
         <div className="flex gap-2 justify-center items-center">
           <p>Appointment Date:</p>
-          <p>12 Jan 2020</p>
-          <p className="font-bold">(10:10)</p>
+          <p>{formatDate(data.appointmentDate)}</p>
+          <p className="font-bold">10:10</p>
         </div>
         <div className="flex gap-2 m-2 p-2 items-center justify-center">
           <div className="bg-blue-700 p-1 rounded">
@@ -38,13 +49,44 @@ const AppointmentCard = () => {
               height="16"
             />
           </div>
-          <p>Video Meeting on</p>
-          <a href="https://meet.google.com/fkx-eevg-fyz">Google Meet</a>
+          <div>
+            {data.meeting == "video" ? (
+              <>
+                <p>Video Meeting on</p>
+                <a href="https://meet.google.com/fkx-eevg-fyz">Google Meet</a>
+              </>
+            ) : (
+              <>
+              <p>Hospital Address :</p>
+              <p>{data.doctor.hospital}</p>
+              </>
+            )}
+          </div>
         </div>
 
         <div className="flex justify-center gap-2 items-center p-2">
           <p>Appointment Status:</p>
-          <p className="bg-red-600 p-1 rounded text-red-950 font-bold">Pending</p>
+          {data.status == "pending" && (
+            <p className="bg-red-600 p-1 rounded text-red-950 font-bold">
+              Pending
+            </p>
+          )}
+          {data.status == "missed" && (
+            <p className="bg-red-600 p-1 rounded text-red-950 font-bold">
+              Missed
+            </p>
+          )}
+          {data.status == "attended" && (
+            <p className="bg-green-600 p-1 rounded text-green-950 font-bold">
+              Attended
+            </p>
+          )}
+          {data.status == "cancelled" && (
+            <p className="bg-red-600 p-1 rounded text-red-950 font-bold">
+              Cancelled
+            </p>
+          )}
+          
         </div>
       </div>
     </div>
--- frontend/src/Dashboard/user_account/Doctorsapp.jsx
+++ frontend/src/Dashboard/user_account/Doctorsapp.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import Loading from '../../components/Loader/Loading';
-import { BASE_URL } from '../../../config';
-import useFetchData from '../../hooks/usefetchData';
-import Error from '../../components/Error/Error';
-import AppointmentCard from './AppointmentCard';
+import Loading from "../../components/Loader/Loading";
+import { BASE_URL } from "../../../config";
+import useFetchData from "../../hooks/usefetchData";
+import Error from "../../components/Error/Error";
+import AppointmentCard from "./AppointmentCard";
 const Doctorsapp = () => {
   const [data, loading, error] = useFetchData(`${BASE_URL}/book/user`);
-  console.log(data)
-  if(loading){
-    return <Loading/>
+  if (error) {
+    return <Error errMessage={error} />;
   }
-  if(error){
-    return <Error errMessage={error}/>
+  if (loading) {
+    return <Loading />;
   }
   return (
-    <div className='container'>
-      <AppointmentCard/>
+    <div className="container">
+      {data && data.length > 0 ? (
+        data.map((item) => (
+          <div key={item._id}>
+            <AppointmentCard data={item} />
+          </div>
+        ))
+      ) : (
+        <div className="flex w-full justify-center items-center">
+          <h1>No Appointments</h1>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default Doctorsapp
+export default Doctorsapp;
