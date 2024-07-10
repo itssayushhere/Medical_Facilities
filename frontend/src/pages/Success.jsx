ab4c458924d20322ba051a7a87d7c64b9131008d
itssayushhere <yayushman088@gmail.com>
2024-07-10 22:52:55 +0530

    updated

--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -3,13 +3,13 @@ const Success = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-[670px] mx-auto">
+    <div className=" mx-auto">
       <div className=" mt-2 flex flex-col items-center justify-center gap-3">
         <span className=" text-4xl font-semibold font-mono">THANK YOU!</span>
-        <span className=" text-balance text-center">
+        <span className=" text-center">
           we are getting started on your order right away, and you will receive
           on order confirmation email shortly.In the meantime ,explore the page
-          for ordering anthing else
+          for ordering anthing else.
         </span>
         <button
           className="bg-gray-800 text-white p-3 rounded"
