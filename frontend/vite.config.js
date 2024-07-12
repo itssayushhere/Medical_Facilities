a3350ec5993672d169d02ca1be500404bf017ea4
itssayushhere <yayushman088@gmail.com>
2024-07-13 04:32:20 +0530

    commited

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { terser } from 'rollup-plugin-terser';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Options to optimize caching
     chunkSizeWarningLimit: 500,
     assetsInlineLimit: 0,
     rollupOptions: {
@@ -21,6 +14,16 @@ export default defineConfig({
             return 'vendor';
           }
         },
+      },
+      plugins: [
+        terser({
+          compress: {
+            drop_console: true, // Optional: removes console logs
+          },
+        }),
+      ],
+      treeshake: {
+        moduleSideEffects: 'no-external', // Enable tree-shaking
       },
     },
   },
