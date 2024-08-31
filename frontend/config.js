254204f7b5d143e5b90b1e86c068d3b20ad93808
itssayushhere <yayushman088@gmail.com>
2024-08-31 16:58:38 +0530

    updated

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://medicalservices-dbyu.onrender.com/api/v1";
+export const BASE_URL = "https://medicalfacilities-production.up.railway.app/api/v1";
 export const token = localStorage.getItem("token");
 export const role = localStorage.getItem("role");
